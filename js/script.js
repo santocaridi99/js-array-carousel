c149f9d47cdfc165bc76aa7ec0e3566bf72651a3
santocaridi99 <santocaridi99@gmail.com>
2021-12-14 15:25:30 +0100

    creazione file e aggiunto commenti per il ragionamento

---
+++ js/script.js
@@ -0,0 +1,20 @@
+//dichiaro array dei titoli
+
+//dichiaro array dei miei paragrafi
+
+//inizializzo un indice a 0
+//indice in questo caso è uguale per entrambi gli  array
+
+//dichiaro due variabili costanti una per  titoli e una per paragrafi
+//imporre che le variabili.innerHtml sono uguali all indice corrente del mio array
+
+
+//dichiaro altre due variabili per i due bottoni di back e next
+
+//azione bottone next ad ogni click
+//con index++
+//variabile.innerhtml = indice corrente
+
+//azione bottone back ad ogni click
+//con index--
+//variabile.innerhtml = indice corrente
